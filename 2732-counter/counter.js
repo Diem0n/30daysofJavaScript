f629ee0a5db1b131eb2d4aedcde3daa92d564907
Talha <90450179+Diem0n@users.noreply.github.com>
2024-04-12 17:18:39 +0500

    Time: 41 ms (94.76%) | Memory: 48.9 MB (42.45%) - LeetSync

--- 2732-counter/counter.js
+++ 2732-counter/counter.js
@@ -4,16 +4,8 @@
  */
 
 var createCounter = function(n) {
-    let cache = null    
     return function() {
-        if (cache == null){
-            cache = n
-            return n
-        }
-        if (cache != null){
-            cache = cache + 1
-            return cache
-        }
+        return n++
     };
 };
 
