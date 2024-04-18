bb513505091699ffc72d09400ce5d8e20570239b
Talha <90450179+Diem0n@users.noreply.github.com>
2024-04-18 20:29:23 +0500

    Time: 54 ms (90.98%) | Memory: 49.5 MB (47.28%) - LeetSync

--- 2859-add-two-promises/add-two-promises.js
+++ 2859-add-two-promises/add-two-promises.js
@@ -4,10 +4,8 @@
  * @return {Promise}
  */
 var addTwoPromises = async function(promise1, promise2) {
-    const p1 = await promise1
-    const p2 = await promise2
-
-    return p1+p2
+    const [p1 , p2] = await Promise.all([promise1, promise2])
+    return p1 + p2
 };
 
 /**
