6be639d107510cef93d4ee0fee9ea9a0385bbdcc
Talha <90450179+Diem0n@users.noreply.github.com>
2024-04-21 23:31:33 +0500

    Time: 68 ms (39.39%) | Memory: 49.5 MB (53.83%) - LeetSync

--- 2821-timeout-cancellation/timeout-cancellation.js
+++ 2821-timeout-cancellation/timeout-cancellation.js
@@ -7,8 +7,7 @@
 var cancellable = function(fn, args, t) {
    var timeout = setTimeout(()=>fn(...args),t);
 
-    var cancelFn = ()=> clearTimeout(timeout);
-    return cancelFn;
+    return  ()=> clearTimeout(timeout);
 };
 
 /**
